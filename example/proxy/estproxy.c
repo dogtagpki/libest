81ab2fd046978072bc07ad0c70b163fc401356c7
Fraser Tweedale <ftweedal@redhat.com>
2022-08-16 16:29:44 +1000

    handle removed FIPS_mode_set() in OpenSSL >= 3.0
    OpenSSL 3.0 removed FIPS_mode_set() subroutine.  FIPS behaviour is
    now controlled by system setting and cannot be controlled via the
    OpenSSL API.
    
    So now, when FIPS mode is requested but FIPS_mode() returns false,
    exit with an error.

--- example/proxy/estproxy.c
+++ example/proxy/estproxy.c
@@ -590,17 +590,12 @@ int main (int argc, char **argv)
             listen_port = atoi(optarg);
             break;
         case 'f':
-            /*
-             * Turn FIPS on if user requested it and exit if failure
-             */
-            set_fips_return = FIPS_mode_set(1);
-            if (set_fips_return != 1) {
-                set_fips_error = ERR_get_error();
-                printf("\nERROR WHILE SETTING FIPS MODE ON exiting ....\n");
+            if (!FIPS_mode()) {
+                printf("\nERROR FIPS required, but disabled...\n");
                 exit(1);
             } else {
                 printf("\nRunning EST Sample Proxy with FIPS MODE = ON !\n");
-            }
+            };
             break;
         default:
             show_usage_and_exit();
